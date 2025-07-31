01485cb40268bb36380c1991686b8eefe7911640
Michael Peters Jr <michael@michaelpeterswa.com>
2025-07-31 01:17:47 -0700

    test: reenable commented tests

--- hand/hand_test.go
+++ hand/hand_test.go
@@ -13,62 +13,62 @@ func TestValue(t *testing.T) {
 		cards          []card.Card
 		expectedValues []int
 	}{
-		// {
-		// 	name: "Ace and Ten",
-		// 	cards: []card.Card{
-		// 		{Rank: card.RankAce, Suit: card.SuitHearts},
-		// 		{Rank: card.RankTen, Suit: card.SuitDiamonds},
-		// 	},
-		// 	expectedValues: []int{11, 21},
-		// },
-		// {
-		// 	name: "Ten and Ace",
-		// 	cards: []card.Card{
-		// 		{Rank: card.RankTen, Suit: card.SuitDiamonds},
-		// 		{Rank: card.RankAce, Suit: card.SuitHearts},
-		// 	},
-		// 	expectedValues: []int{11, 21},
-		// },
-		// {
-		// 	name: "Two Aces",
-		// 	cards: []card.Card{
-		// 		{Rank: card.RankAce, Suit: card.SuitHearts},
-		// 		{Rank: card.RankAce, Suit: card.SuitDiamonds},
-		// 	},
-		// 	expectedValues: []int{2, 12},
-		// },
-		// {
-		// 	name: "Ten and King",
-		// 	cards: []card.Card{
-		// 		{Rank: card.RankTen, Suit: card.SuitHearts},
-		// 		{Rank: card.RankKing, Suit: card.SuitDiamonds},
-		// 	},
-		// 	expectedValues: []int{20},
-		// },
-		// {
-		// 	name: "Two and Three",
-		// 	cards: []card.Card{
-		// 		{Rank: card.RankTwo, Suit: card.SuitHearts},
-		// 		{Rank: card.RankThree, Suit: card.SuitDiamonds},
-		// 	},
-		// 	expectedValues: []int{5},
-		// },
-		// {
-		// 	name: "Single Ace",
-		// 	cards: []card.Card{
-		// 		{Rank: card.RankAce, Suit: card.SuitHearts},
-		// 	},
-		// 	expectedValues: []int{1, 11},
-		// },
-		// {
-		// 	name: "Ace Hits",
-		// 	cards: []card.Card{
-		// 		{Rank: card.RankAce, Suit: card.SuitHearts},
-		// 		{Rank: card.RankTwo, Suit: card.SuitDiamonds},
-		// 		{Rank: card.RankKing, Suit: card.SuitClubs},
-		// 	},
-		// 	expectedValues: []int{13},
-		// },
+		{
+			name: "Ace and Ten",
+			cards: []card.Card{
+				{Rank: card.RankAce, Suit: card.SuitHearts},
+				{Rank: card.RankTen, Suit: card.SuitDiamonds},
+			},
+			expectedValues: []int{11, 21},
+		},
+		{
+			name: "Ten and Ace",
+			cards: []card.Card{
+				{Rank: card.RankTen, Suit: card.SuitDiamonds},
+				{Rank: card.RankAce, Suit: card.SuitHearts},
+			},
+			expectedValues: []int{11, 21},
+		},
+		{
+			name: "Two Aces",
+			cards: []card.Card{
+				{Rank: card.RankAce, Suit: card.SuitHearts},
+				{Rank: card.RankAce, Suit: card.SuitDiamonds},
+			},
+			expectedValues: []int{2, 12},
+		},
+		{
+			name: "Ten and King",
+			cards: []card.Card{
+				{Rank: card.RankTen, Suit: card.SuitHearts},
+				{Rank: card.RankKing, Suit: card.SuitDiamonds},
+			},
+			expectedValues: []int{20},
+		},
+		{
+			name: "Two and Three",
+			cards: []card.Card{
+				{Rank: card.RankTwo, Suit: card.SuitHearts},
+				{Rank: card.RankThree, Suit: card.SuitDiamonds},
+			},
+			expectedValues: []int{5},
+		},
+		{
+			name: "Single Ace",
+			cards: []card.Card{
+				{Rank: card.RankAce, Suit: card.SuitHearts},
+			},
+			expectedValues: []int{1, 11},
+		},
+		{
+			name: "Ace Hits",
+			cards: []card.Card{
+				{Rank: card.RankAce, Suit: card.SuitHearts},
+				{Rank: card.RankTwo, Suit: card.SuitDiamonds},
+				{Rank: card.RankKing, Suit: card.SuitClubs},
+			},
+			expectedValues: []int{13},
+		},
 		{
 			name: "Bust",
 			cards: []card.Card{
